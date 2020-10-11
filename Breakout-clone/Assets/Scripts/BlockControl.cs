b3ed22ca87f682f555103f8007acc01fec499d4d
SonaruIsuge <bread511206@gmail.com>
2020-10-12 00:28:48 +0800

    Add files

---
+++ Breakout-clone/Assets/Scripts/BlockControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockControl : MonoBehaviour
+{
+    BallControl B_ball = new BallControl();
+    public GameObject ball;
+    void Start()
+    {
+        UIControl.s_brickCount++;
+        B_ball = ball.GetComponent<BallControl>();
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        B_ball.score += 10;
+        UIControl.s_brickCount--;
+        Destroy(gameObject);
+    }
+}
---
+++ Breakout-clone/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+    void FixedUpdate()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * hor * speed;
+    }
+}
